1f0da7fd961b75b5ce6ee3bac69c8056a270f254
Thomas Weibel <thom@beeblebrox.net>
2008-08-30 14:26:01 +0200

    Added configuration for rhtml mode

--- config/32rails.el
+++ config/32rails.el
@@ -8,3 +8,10 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/rinari/")
 (require 'rinari)
 (global-set-key (kbd "C-x C-M-f") 'find-file-in-project) ;; optional
+
+;; rhtml-mode
+(add-to-list 'load-path "~/.emacs.d/vendor/rhtml")
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook
+ 	  (lambda () (rinari-launch)))
+(add-to-list 'auto-mode-alist '("\\.html\\.erb$" . rhtml-mode))
