699916434f4fbb81c936e5d702cfe591cbb3963d
Thomas Weibel <thom@beeblebrox.net>
2008-08-30 14:33:58 +0200

    Added configuration for Python mode

---
+++ config/36python.el
@@ -0,0 +1,12 @@
+;;; 46python.el --- Custom python-mode configuration
+
+;; Python mode
+(require 'python)
+(add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+(add-to-list 'auto-mode-alist '("\\(^\\|/\\)SCons" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
+;;(define-key py-mode-map [backspace] 'py-electric-backspace)
+;;(define-key py-mode-map "\C-h"      'py-electric-backspace)
+;;(define-key py-mode-map [delete]    'py-electric-delete)
+;;(define-key py-mode-map "\C-d"      'py-electric-delete)
