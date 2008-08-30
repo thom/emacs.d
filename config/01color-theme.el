ea2ddea871d826a4414cb05f9759122b5d7b4aec
Thomas Weibel <thom@beeblebrox.net>
2008-08-30 11:32:53 +0200

    Use twilight-emacs color theme

---
+++ config/01color-theme.el
@@ -0,0 +1,8 @@
+;; Configure color theme
+
+(require 'color-theme)
+
+(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
+(color-theme-twilight)
+
+(setq color-theme-is-global t)
