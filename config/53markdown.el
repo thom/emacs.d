48e521633f739723e95565bca2a677e19ee6db5c
Thomas Weibel <thom@beeblebrox.net>
2008-08-30 14:47:02 +0200

    Added configuration for markdown mode

---
+++ config/53markdown.el
@@ -0,0 +1,5 @@
+; markdown
+(add-to-list 'load-path "~/.emacs.d/markdown-mode")
+(autoload 'markdown-mode "markdown-mode.el"
+   "Major mode for editing Markdown files" t)
+(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
