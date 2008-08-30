3ee8b813ffff82eccd47828b6ea2a053555b5746
Thomas Weibel <thom@beeblebrox.net>
2008-08-30 14:54:52 +0200

    Added miscellaneous definitions

---
+++ config/90misc.el
@@ -0,0 +1,21 @@
+;; Miscellaneous definitions 
+
+;; Browse kill-ring
+(require 'browse-kill-ring)
+(defadvice yank-pop (around kill-ring-browse-maybe (arg) activate)
+  "If last action was not a yank, run `browse-kill-ring' instead."
+  (if (not (eq last-command 'yank))
+      (browse-kill-ring)
+    ad-do-it))
+
+;; Lorem ipsum dolor sit amet
+(defun lorem ()
+  "Insert a lorem ipsum."
+  (interactive)
+  (insert "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
+	  "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad "
+	  "minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
+	  "aliquip ex ea commodo consequat. Duis aute irure dolor in "
+	  "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+	  "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
+	  "culpa qui officia deserunt mollit anim id est laborum."))
